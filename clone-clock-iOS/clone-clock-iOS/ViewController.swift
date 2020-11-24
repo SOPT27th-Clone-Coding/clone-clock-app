4a437fc176a58ebc6ef243da1f13536c889bc8ea
inaeLee <inae9709@hanmail.net>
2020-11-25 00:05:48 +0900

    init commit

---
+++ clone-clock-iOS/clone-clock-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clone-clock-iOS
+//
+//  Created by inae Lee on 2020/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
