de0ee2b65741507c320d265fd1adb3d9e72af59b
inaeLee <inae9709@hanmail.net>
2020-11-27 19:24:20 +0900

    add: 세계시계 레이아웃 잡기

---
+++ clone-clock-iOS/clone-clock-iOS/Source/Cell/ClockCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClockCell.swift
+//  clone-clock-iOS
+//
+//  Created by inae Lee on 2020/11/27.
+//
+
+import UIKit
+
+class ClockCell: UITableViewCell {
+    static let identifier = "ClockCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- clone-clock-iOS/clone-clock-iOS/Source/VC/ClockVC.swift
+++ clone-clock-iOS/clone-clock-iOS/Source/VC/ClockVC.swift
@@ -8,22 +8,77 @@
 import UIKit
 
 class ClockVC: UIViewController {
-
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var clockTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setView()
+        
+        clockTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
+extension ClockVC {
+    func setView() {
+        clockTableView.backgroundColor = .black
+        
+        // navigation bar
+        let editButton = UIBarButtonItem(title: "편집", style: .plain, target: self, action: nil)
+        editButton.tintColor = .systemOrange
+        editButton.setTitleTextAttributes([.font: UIFont.boldSystemFont(ofSize: 16)], for: .normal)
+         
+        let plusButton = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: nil)
+        plusButton.tintColor = .systemOrange
+        
+        navigationItem.leftBarButtonItem = editButton
+        navigationItem.rightBarButtonItem = plusButton
+        
+        navigationBar.setItems([navigationItem], animated: true)
+        navigationBar?.topItem?.title = .none
+        navigationBar.barTintColor = .black
+        navigationBar.isTranslucent = false
+        
+//         table view
+        let header = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 50))
+        header.backgroundColor = .black
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let headerLabel = UILabel(frame: header.bounds)
+        headerLabel.text = "세계 시계"
+        headerLabel.font = .boldSystemFont(ofSize: 30)
+        headerLabel.textAlignment = .left
+        headerLabel.textColor = .white
+
+        header.addSubview(headerLabel)
+
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        headerLabel.leadingAnchor.constraint(equalTo: header.leadingAnchor, constant: 18).isActive = true
+        headerLabel.centerYAnchor.constraint(equalTo: header.centerYAnchor).isActive = true
+        clockTableView.tableHeaderView = header
+        
+        clockTableView.separatorColor = .systemGray
     }
-    */
+}
 
+extension ClockVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ClockCell") as? ClockCell else {return UITableViewCell()}
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+}
+
+extension ClockVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        clockTableView.deselectRow(at: indexPath, animated: true)
+    }
 }
