4d129567b02040ed5f8fa11f2cb25dd98fd31137
inaeLee <inae9709@hanmail.net>
2020-12-12 01:17:52 +0900

    add: repeatCell check button (accessoryView) 추가

--- clone-clock-iOS/clone-clock-iOS/Source/VC/Alarm/RepeatVC.swift
+++ clone-clock-iOS/clone-clock-iOS/Source/VC/Alarm/RepeatVC.swift
@@ -8,10 +8,58 @@
 import UIKit
 
 class RepeatVC: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    var weeksData : [(date: String, selected: Bool)] = [("월", false), ("화", false), ("수", false), ("목", false), ("금", false), ("토", false), ("일", false)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setView()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension RepeatVC {
+    func setView() {
+        tableView.backgroundColor = #colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 1)
+        tableView.separatorColor = .gray
+        
+        tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        tableView.tableFooterView?.backgroundColor = .clear
+    }
+}
+
+extension RepeatVC :UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        weeksData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = "\(weeksData[indexPath.row].date)요일마다"
+        cell.textLabel?.textColor = .white
+        cell.backgroundColor = .darkGray
+        
+        return cell
+    }
+}
+
+extension RepeatVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else {return}
+        cell.tintColor = .orange
+        
+        let week = weeksData[indexPath.row]
+        weeksData.remove(at: indexPath.row)
+        weeksData.insert((week.date, !week.selected), at: indexPath.row)
+        
+        if week.selected {
+            cell.accessoryType = .none
+        } else {
+            cell.accessoryType = .checkmark
+        }
     }
 }
