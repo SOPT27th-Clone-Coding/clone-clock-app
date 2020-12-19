e2d86ce97f28d9c0a26947eefa307623fabb2bd8
inaeLee <inae9709@hanmail.net>
2020-12-19 23:32:05 +0900

    add: AddSoundVC segue 연결

---
+++ clone-clock-iOS/clone-clock-iOS/Source/VC/Timer/AddSoundVC.swift
@@ -0,0 +1,29 @@
+//
+//  AddSoundVC.swift
+//  clone-clock-iOS
+//
+//  Created by inae Lee on 2020/12/19.
+//
+
+import UIKit
+
+class AddSoundVC: UIViewController {
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var soundTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setView()
+    }
+}
+
+extension AddSoundVC {
+    func setView() {
+        view.backgroundColor = .black
+        
+        //navigation bar
+        self.navigationBar.barStyle = .black
+        self.navigationBar.topItem?.title = "타이머 종료 시"
+    }
+}
