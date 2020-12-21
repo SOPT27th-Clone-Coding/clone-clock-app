76ba4dfedd50ea9c442eae19a8e67948b552a3a9
inaeLee <inae9709@hanmail.net>
2020-12-22 01:03:04 +0900

    add: navigation bar 취소, 설정 버튼 및 동작  추가

--- clone-clock-iOS/clone-clock-iOS/Source/VC/Timer/AddSoundVC.swift
+++ clone-clock-iOS/clone-clock-iOS/Source/VC/Timer/AddSoundVC.swift
@@ -10,11 +10,19 @@ import UIKit
 class AddSoundVC: UIViewController {
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var soundTableView: UITableView!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var settingButton: UIBarButtonItem!
+    
+    var soundData: [Data] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setView()
+    }
+    
+    @IBAction func touchUpCancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
 
@@ -23,7 +31,13 @@ extension AddSoundVC {
         view.backgroundColor = .black
         
         //navigation bar
-//        self.navigationBar.barStyle = .blackTranslucent
+        self.navigationBar.barStyle = .black
         self.navigationBar.topItem?.title = "타이머 종료 시"
+        
+        cancelButton.tintColor = .systemOrange
+        cancelButton.title = "취소"
+        settingButton.tintColor = .systemOrange
+        settingButton.title = "설정"
     }
 }
+ 
